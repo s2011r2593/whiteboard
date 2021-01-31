b36086b6376fcf76eb7beb4b1b96d7874469308b
Sean Rhee <seanrhee2024@u.northwestern.edu>
2021-01-31 04:44:53 -0500

    gaussian elimination

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub mod lin_alg {
+  pub fn gauss_jordan(mut aug: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let length: usize = aug.len();
+        
+    for i in 0..(length - 1) {
+      for j in (i+1)..length {
+        let co_i = aug[i][i];
+        let co_j = aug[j][i];
+        for k in i..(length + 1) {
+          aug[j][k] -= (co_j / co_i) * aug[i][k];
+        }
+      }
     }
+    for i in (0..length).rev() {
+      let co_i: f64 = aug[i][i];
+      aug[i][i] /= co_i;
+      aug[i][length] /= co_i;
+      for j in 0..i {
+        let co_j: f64 = aug[j][i];
+        aug[j][length] -= co_j * aug[i][length];
+      }
+    }
+       
+    aug
+  }
 }
