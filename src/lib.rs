889c4b9f8d78c60b4325f3eb3142917be93ca720
Sean Rhee <seanrhee2024@u.northwestern.edu>
2021-02-01 21:36:22 -0500

    added functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+pub mod trig {
+  pub fn get_unit_vec(a: Vec<f64>) -> Vec<f64> {
+    let mut mag: f64 = 0.0;
+    for i in a.iter() {
+      mag += i*i;
+    }
+    a.iter().map(|x| x / mag.sqrt()).collect::<Vec<f64>>()
+  }
+
+  pub fn get_theta(a: Vec<f64>, b: Vec<f64>) -> f64 {
+    dot(a, b).acos()
+  }
+
+  pub fn dot(a: Vec<f64>, b: Vec<f64>) -> f64 {
+    let mut res: f64 = 0.0;
+    for (i, j) in a.iter().zip(b.iter()) {
+      res += i * j;
+    }
+    res
+  }
+}
+
 pub mod lin_alg {
   pub fn gauss_jordan(mut aug: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let length: usize = aug.len();
@@ -22,5 +44,14 @@ pub mod lin_alg {
     }
        
     aug
+  }
+}
+
+pub mod misc {
+  pub fn rotate(point: (f64, f64), center: (f64, f64), theta: f64) -> (f64, f64) {
+    (
+      (point.0 - center.0) * theta.cos() - (point.1 - center.1) * theta.sin() + center.0,
+      (point.0 - center.0) * theta.sin() + (point.1 - center.1) * theta.cos() + center.1
+    )
   }
 }
